1350a3bcfbaf82713269c0bf743efdd224bc834f
MGIC12 <mager.infante@gmail.com>
2025-08-14 22:06:22 -0300

    Ejercicio 2 terminado

--- TP1/Ejercicio2/Ejercicio2.php
+++ TP1/Ejercicio2/Ejercicio2.php
@@ -7,24 +7,25 @@
 </head>
 <body>
     <h1>TP1 - Ejercicio 2</h1>
-    <h2>Horas de cursada</h2>
+    <hr>
+    <h2>Horas de cursada PWD</h2>
     <form action="verhoras.php" method="get">
-        <label for="horas">Horas lunes</label>
+        <label for="horas">Horas Lunes: </label>
         <input type="number" name="horasL" id="horasL">
-        <br>
-        <label for="horas">Horas martes</label>
+        <br><br>
+        <label for="horas">Horas Martes: </label>
         <input type="number" name="horasM" id="horasM">
-        <br>
-        <label for="horas">Horas miercoles</label>
+        <br><br>
+        <label for="horas">Horas Miércoles: </label>
         <input type="number" name="horasMi" id="horasMi">
-        <br>
-        <label for="horas">Horas jueves</label>
+        <br><br>
+        <label for="horas">Horas Jueves: </label>
         <input type="number" name="horasJ" id="horasJ">
-        <br>
-        <label for="horas">Horas viernes</label>
+        <br><br>
+        <label for="horas">Horas Viernes: </label>
         <input type="number" name="horasV" id="horasV">
-        <br>
-        <button type="submit">Obtener horas</button>
+        <br><br>
+        <button type="submit">Obtener horas por día</button>
     </form>
 </body>
 </html>
--- TP1/Ejercicio2/verhoras.php
+++ TP1/Ejercicio2/verhoras.php
@@ -8,13 +8,13 @@ $horasPWDV = $_GET["horasV"];
 $horasPWD = array($horasPWDL, $horasPWDM, $horasPWDMi, $horasPWDJ, $horasPWDV);
 $dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
 
-$horasPWDarray = "Horas cursadas por día: <br>";
+$horasPWDarray = "<h1>Horas cursadas por día:</h1><hr><br>";
 for ($i = 0; $i < count($horasPWD); $i++){
-    $horasPWDarray .= $dias[$i] . ": " . $horasPWD[$i] . "<br>";
+    $horasPWDarray .= $dias[$i] . ": " . $horasPWD[$i] . " horas <br>";
 }
 
 $horasTotal = $horasPWDL + $horasPWDM + $horasPWDMi + $horasPWDJ + $horasPWDV;
 echo $horasPWDarray . "<br>";
-echo "El total de horas cursadas es: " . $horasTotal;
+echo "El total de horas cursadas por semana es de: " . $horasTotal." horas";
 
 ?>
