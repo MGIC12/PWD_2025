6e89432122f65f8a9e8685b76868b78aa4082644
MGIC12 <mager.infante@gmail.com>
2025-08-28 12:25:22 -0300

    Ejercicio 3 TP2 terminado

--- TP2/Ejercicio3/Ejercicio3.php
+++ TP2/Ejercicio3/Ejercicio3.php
@@ -4,19 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://unpkg.com/just-validate@latest/dist/just-validate.production.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"></link>
     <title>Ejercicio 3 - Login</title>
 </head>
-<body>
-    <h1>TP2 - Ejercicio 3</h1>
-    <form action="verificaPass.php" method="post" id="form">
-        <label for="user">Ingrese su usuario: </label>
-        <input type="text" name="user" id="user">
-        <br><br>
-        <label for="pass">Ingrese su contraseña: </label>
-        <input type="password" name="pass" id="pass">
-        <br><br>
-        <input type="submit" value="Enviar">
-    </form>
+<body class="d-flex justify-content-center bg-secondary text-center">
+    <form class="container bg-light p-5 align-items-center justify-content-center mt-5 w-25 rounded-2" action="verificaPass.php" method="post" id="form">
+    <h2 class="text-center text-secondary fw-normal pb-4">Member Login</h2>
+    <input type="text" name="user" id="user" placeholder="Username" class="form-control w-75 mx-auto rounded-1">
+    <br>
+    <input type="password" name="pass" id="pass" placeholder="Password" class="form-control w-75 mx-auto rounded-1">
+    <br>
+    <input type="submit" value="Login" class="btn btn-success w-75 mx-auto rounded-1">
+</form>
     <script>
     const validate = new window.JustValidate('#form');
     validate
@@ -52,5 +51,6 @@
             document.getElementById('form').submit();
         });
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
