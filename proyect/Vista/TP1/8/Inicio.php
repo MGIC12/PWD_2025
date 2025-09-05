28f3398fe7dd33ea958210b2a14d3558ca3d9881
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2025-09-05 19:00:14 -0300

    fixed style

--- proyect/Vista/TP1/8/Inicio.php
+++ proyect/Vista/TP1/8/Inicio.php
@@ -6,7 +6,7 @@
   <title>Ejercicio 8</title>
   <link rel="stylesheet" href="../../Frameworks/bootstrap.min.css">
 </head>
-<body class="bg-light d-flex flex-column min-vh-100">
+<body class="d-flex flex-column min-vh-100">
 
   <!-- Navbar -->
   <?php
